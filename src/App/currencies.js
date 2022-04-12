f62f0ee7f99e1861e6c3251d7d426a89e8f7cac2
Damian Michalowski <dowski888@gmail.com>
2022-04-13 01:19:17 +0200

    currencies array file added

---
+++ src/App/currencies.js
@@ -0,0 +1,19 @@
+
+export const currencies = [
+    {
+        name: "EUR",
+        rate: 4.6418,
+    },
+    {
+        name: "USD",
+        rate: 4.2752,
+    },
+    {
+        name: "CHF",
+        rate: 4.59,
+    },
+    {
+        name: "BTC",
+        rate: 171134.26,
+    },
+];
