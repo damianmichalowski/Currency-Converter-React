3f295ad627114e775f4abdef4f6b6baad4357e13
Damian Michalowski <dowski888@gmail.com>
2022-04-24 17:09:29 +0200

    format date funct taken above Clock

--- src/App/Form/Clock/index.js
+++ src/App/Form/Clock/index.js
@@ -1,21 +1,21 @@
 import { Wrapper } from "./styled";
 import { useCurrentDate } from "./useCurrentDate";
 
+const formatdate = (date) => {
+    return date.toLocaleString(
+         undefined,
+         {
+             weekday: "long",
+             day: "numeric",
+             month: "long",
+             minute: "numeric",
+             hour: "numeric",
+             second: "numeric"
+         });
+ };
+
 export const Clock = () => {
     const date = useCurrentDate();
-
-    const formatdate = (date) => {
-       return date.toLocaleString(
-            undefined,
-            {
-                weekday: "long",
-                day: "numeric",
-                month: "long",
-                minute: "numeric",
-                hour: "numeric",
-                second: "numeric"
-            });
-    };
 
     return (
         <Wrapper>
